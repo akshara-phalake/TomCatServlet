79105ff9bb17642d607b85d7ab5613c9f58bf36a
“akshara-phalake” <akshara.phalake18@gmail.com>
2022-05-11 08:19:45 +0530

    Added: Doc 2 Login servlet

---
+++ FirstServletProject/src/main/java/com/examples/servletproject/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.examples.servletproject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+        description = "Login Servlet Testing",
+        urlPatterns = { "/LoginServlet" },
+        initParams = {
+                @WebInitParam(name = "user", value = "Akshara"),
+                @WebInitParam(name = "password", value = "Aks@74478")
+        }
+)
+
+public class LoginServlet extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
+        String userID = getServletConfig().getInitParameter("user");
+        String password = getServletConfig().getInitParameter("password");
+//        String nameRegex = "^[A-Z][a-z]{2,}";
+//        String pwdRegex = "^(?=.*[0-9])(?=[^@#$%^&+=]*[@#$%^&+=][^@#$%^&+=]*$)(?=.*[a-z])(?=.*[A-Z]).{8,}$";
+
+//        if(userID.equals(user) && userID.matches(nameRegex) && password.equals(pwd) && password.matches(pwdRegex)){
+        if(userID.equals(user) && password.equals(pwd)){
+            request.setAttribute("user",user);
+            request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+        } else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out  = response.getWriter();
+            out.println("<font color = red> Either username or password is wrong</font>");
+            rd.include(request, response);
+        }
+    }
+}
